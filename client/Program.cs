4b9673c2b7f8a3dcf4eab40b3256257e49f8948d
Will Figtree <w.figtree@gmail.com>
2020-08-04 11:30:49 +1000

    initial commit

---
+++ client/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace client
+{
+    class Program
+    {
+    static string reqConnectionString = ">tcp://localhost:5556";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ZeroMQ client");
+
+            // connect to the server
+            Console.WriteLine("connecting to server...");
+            using (var req = new RequestSocket(reqConnectionString))
+            {
+                Console.WriteLine("send msg: ");
+                
+                while (true)
+                {
+                    // send message
+                    var tx = Console.ReadLine();
+                    var txBytes = Encoding.UTF8.GetBytes(tx);
+                    req.SendFrame(txBytes);
+                    Console.WriteLine("sent: " + tx);
+
+                    // wait for echo response
+                    var rxBytes = req.ReceiveFrameBytes();
+                    var rx = Encoding.UTF8.GetString(rxBytes);
+                    Console.WriteLine("got: " + rx);
+                }
+            }
+        }
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using NetMQ.Sockets;
+using NetMQ;
+using System.Text;
+
+// independent server process
+// creates a data stream on launch (pub/sub)
+// also responds to a simple echo request (synchronous request/response)
+
+// https://netmq.readthedocs.io/en/latest/poller/
+
+namespace server
+{
+    class Program
+    {
+        static string respConnectionString = "@tcp://localhost:5556";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ZeroMQ server");
+
+
+            // setup response socket for request/response operations        
+            var resp = new ResponseSocket(respConnectionString);
+            resp.ReceiveReady += ResponseHandler;
+            
+            while(true)
+            {
+                //Task.Delay(1000).Wait();
+            }
+        }
+
+        // Invoked whenever atleast one message is ready for the response socket
+        static void ResponseHandler(object s, NetMQSocketEventArgs e)
+        {
+            while (e.Socket.TryReceiveFrameBytes(out var frame))
+            {
+                // Interpret frame data
+                var rx = Encoding.UTF8.GetString(frame, 0, frame.Length);
+                Console.WriteLine("got: " + rx);
+                 
+                 // Send an echo
+                e.Socket.SendFrame(frame);
+                Console.WriteLine("sent: " + rx);
+            }
+        }
+
+    }
+}
